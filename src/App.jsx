38ccd07e8b19ae559d44d3f9bb1f55445c34c858
Asutosh-1234 <pandaashutosh220@gmail.com>
2026-06-06 09:39:11 +0530

    day 21

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,41 @@
+import { useEffect, useState } from 'react'
+import { useDispatch } from 'react-redux'
 import './App.css'
+import authService from './appwrite/auth'
+import { login, logout } from './store/authSlice'
+import { Footer, Header } from './components'
+
 
 function App() {
-  console.log();
-  
+  const [loading, setLoading] = useState(true)
+  const dispatch = useDispatch()
+
+  useEffect(()=>{
+    authService.getCurrentUser().then((userData)=>{
+      if(userData){
+        dispatch(login({userData}))
+      }else{
+        dispatch(logout())
+      }
+    }).finally( ()=> setLoading(false))
+  },[])
 
   return (
-    <>
-      <h1>Hello from Blog Application</h1>
-    </>
+    !loading? (
+      <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+        <div>
+          <main>
+            {/* TODO: <Outlet/> */}
+          </main>
+          <Header />
+          <Footer/>
+        </div>
+      </div>
+    ):(
+      <div>
+
+      </div>
+    )
   )
 }
 
